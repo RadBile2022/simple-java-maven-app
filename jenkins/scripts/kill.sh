974ac6f18f139f6fac64e55b467677ea302f8cdb
Radar <102400087+RadBile2022@users.noreply.github.com>
2023-11-22 15:52:49 +0700

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,3 +4,5 @@ echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
 kill $(cat .pidfile)
+pid=$(pgrep -f "java -jar nama_file.jar")
+kill $pid
