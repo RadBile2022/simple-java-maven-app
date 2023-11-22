fbd226b0f18f31c60e06b1111e06ef58d21f897f
Radar <102400087+RadBile2022@users.noreply.github.com>
2023-11-22 15:53:54 +0700

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
-echo 'The following command terminates the "maven clean install" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
-pid=$(pgrep -f "java -jar nama_file.jar")
+# echo 'The following command terminates the "maven clean install" process using its PID'
+# echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
+# echo 'was executed.'
+# set -x
+# kill $(cat .pidfile)
+pid=$(pgrep -f "java -jar my-app.jar")
 kill $pid
+echo 'was executed. terminate jar'
