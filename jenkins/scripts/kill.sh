0266798aa19d441ee15f9bd7e99656dd1b2bac0d
Radar <102400087+RadBile2022@users.noreply.github.com>
2023-11-22 15:39:19 +0700

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-echo 'The following command terminates the "npm start" process using its PID'
+echo 'The following command terminates the "maven clean install" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
