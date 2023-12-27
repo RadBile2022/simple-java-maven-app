c8f9bdfa7ecb4e48f4b4d7066e97ba656f06a0af
Radar <102400087+RadBile2022@users.noreply.github.com>
2023-12-27 10:31:35 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    // private static final String MESSAGE = "";
+    private static final String MESSAGE = "Hello World! Kakak";
 
-    // public App() {}
+    public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Adek");
+        System.out.println(MESSAGE);
     }
 
-    // public String getMessage() {
-    //     return MESSAGE;
-    // }
+    public String getMessage() {
+        return MESSAGE;
+    }
 }
